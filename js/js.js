f26f22e98bc8f99c004401ca51d6ac0694f44872
Andrey-Bondarevskij <aloxnesir@gmail.com>
2021-09-14 16:37:55 +0300

    last commit

--- js/js.js
+++ js/js.js
@@ -1,17 +1,17 @@
-const refs = {
-            openModalBtn: document.querySelector("[data-open-modal]"),
-            closeModalBtn: document.querySelector("[data-close-modal]"),
-            backdrop: document.querySelector("[data-backdrop]"),
-        };
-        refs.openModalBtn.addEventListener("click", toggleModal);
-        refs.closeModalBtn.addEventListener("click", toggleModal);
+// const refs = {
+//             openModalBtn: document.querySelector("[data-open-modal]"),
+//             closeModalBtn: document.querySelector("[data-close-modal]"),
+//             backdrop: document.querySelector("[data-backdrop]"),
+//         };
+//         refs.openModalBtn.addEventListener("click", toggleModal);
+//         refs.closeModalBtn.addEventListener("click", toggleModal);
 
-        refs.backdrop.addEventListener("click", logBackdropClick);
+//         refs.backdrop.addEventListener("click", logBackdropClick);
 
-        function toggleModal(){
-            refs.backdrop.classList.toggle("hidden");
-        }
+//         function toggleModal(){
+//             refs.backdrop.classList.toggle("hidden");
+//         }
 
-        function logBackdropClick(){
-            console.log("fffff");
-}
+//         function logBackdropClick(){
+//             console.log("fffff");
+// }
